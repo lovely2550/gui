6f88062f526d494d75e8eb15289c55e3949ca563
lovely2550 <kirtichuphechr0@gmail.com>
2025-06-12 16:30:54 +0700

    Update 04_install.sh

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -1,57 +1,21 @@
-#!/usr/bin/env bash
-#
-# Copyright (c) 2018-present The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#!/bin/bash
+# สคริปต์ติดตั้งสำหรับ CI (Continuous Integration) ที่จะทำงานในขั้นตอน lint
 
-export LC_ALL=C
+set -euxo pipefail
+# เปิดโหมด:
+# -e: ออกจากสคริปต์ถ้ามีคำสั่งล้มเหลว
+# -u: ออก error ถ้าใช้ตัวแปรที่ไม่ได้กำหนด
+# -x: แสดงคำสั่งที่กำลังรัน
+# -o pipefail: ตรวจสอบ error จากทุกคำสั่งใน pipeline
 
-export CI_RETRY_EXE="/ci_retry --"
+sudo apt-get update -y
+# อัพเดตข้อมูลแพ็กเกจในระบบ
 
-pushd "/"
+sudo apt-get install -y shellcheck
+# ติดตั้ง shellcheck เครื่องมือเช็ค syntax shell script
 
-${CI_RETRY_EXE} apt-get update
-# Lint dependencies:
-# - cargo (used to run the lint tests)
-# - curl/xz-utils (to install shellcheck)
-# - git (used in many lint scripts)
-# - gpg (used by verify-commits)
-${CI_RETRY_EXE} apt-get install -y cargo curl xz-utils git gpg
+pip3 install yamllint
+# ติดตั้ง yamllint ผ่าน pip3 เพื่อเช็ค syntax ของไฟล์ YAML
 
-PYTHON_PATH="/python_build"
-if [ ! -d "${PYTHON_PATH}/bin" ]; then
-  (
-    ${CI_RETRY_EXE} git clone --depth=1 https://github.com/pyenv/pyenv.git
-    cd pyenv/plugins/python-build || exit 1
-    ./install.sh
-  )
-  # For dependencies see https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-  ${CI_RETRY_EXE} apt-get install -y build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-    clang
-  env CC=clang python-build "$(cat "/.python-version")" "${PYTHON_PATH}"
-fi
-export PATH="${PYTHON_PATH}/bin:${PATH}"
-command -v python3
-python3 --version
-
-${CI_RETRY_EXE} pip3 install \
-  codespell==2.4.1 \
-  lief==0.16.6 \
-  mypy==1.4.1 \
-  pyzmq==25.1.0 \
-  ruff==0.5.5 \
-  vulture==2.6
-
-SHELLCHECK_VERSION=v0.8.0
-curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | \
-    tar --xz -xf - --directory /tmp/
-mv "/tmp/shellcheck-${SHELLCHECK_VERSION}/shellcheck" /usr/bin/
-
-MLC_VERSION=v0.19.0
-MLC_BIN=mlc-x86_64-linux
-curl -sL "https://github.com/becheran/mlc/releases/download/${MLC_VERSION}/${MLC_BIN}" -o "/usr/bin/mlc"
-chmod +x /usr/bin/mlc
-
-popd || exit
+pip3 install jsonlint
+# ติดตั้ง jsonlint ผ่าน pip3 เพื่อเช็ค syntax ของไฟล์ JSON
